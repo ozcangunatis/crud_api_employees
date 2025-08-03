db853369527e0f4d8d9c2ce28f8578e2ed5717d8
Özcangün Atış <67122794+ozcangunatis@users.noreply.github.com>
2025-08-03 20:15:28 +0300

    Update EmployeeController.java

--- src/main/java/com/exercise/crud/controller/EmployeeController.java
+++ src/main/java/com/exercise/crud/controller/EmployeeController.java
@@ -3,7 +3,7 @@ import com.exercise.crud.model.Employee;
 import com.exercise.crud.service.EmployeeService;
 import org.springframework.http.HttpStatus; import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*; import java.util.List;
-@RestController @RequestMapping("/api/employees") public class EmployeeController {private final EmployeeService employeeService;
+@RestControlle @RequestMapping("/api/employees") public class EmployeeController {private final EmployeeService employeeService;
 public EmployeeController(EmployeeService employeeService){this.employeeService=employeeService;}
 @PostMapping public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee){return new ResponseEntity<>(employeeService.save(employee),HttpStatus.CREATED);}
 @GetMapping public List<Employee> getAllEmployees(){return employeeService.getAllEmployees();}
